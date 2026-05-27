724f409556d4090d50b5c17a7397fd061a12862e
arnehermans08 <arnehermans@leerling.wico.be>
2026-05-27 09:58:03 +0200

    Create sampledata.js

---
+++ sampledata.js
@@ -0,0 +1,177 @@
+[
+  {
+    _id: ObjectId('69fb266389010d71a9e121b8'),
+    id: 1,
+    type: 'az1',
+    waarde: 62,
+    tijd: ISODate('2026-05-06T11:30:43.863Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266689010d71a9e121ba'),
+    id: 1,
+    type: 'ax1',
+    waarde: 41,
+    tijd: ISODate('2026-05-06T11:30:46.127Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266689010d71a9e121bc'),
+    id: 1,
+    type: 'ay1',
+    waarde: 92,
+    tijd: ISODate('2026-05-06T11:30:46.321Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266689010d71a9e121be'),
+    id: 1,
+    type: 'az1',
+    waarde: 68,
+    tijd: ISODate('2026-05-06T11:30:46.524Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266989010d71a9e121c0'),
+    id: 1,
+    type: 'ax1',
+    waarde: 72,
+    tijd: ISODate('2026-05-06T11:30:49.715Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266989010d71a9e121c2'),
+    id: 1,
+    type: 'ay1',
+    waarde: 69,
+    tijd: ISODate('2026-05-06T11:30:49.915Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266a89010d71a9e121c4'),
+    id: 1,
+    type: 'az1',
+    waarde: 63,
+    tijd: ISODate('2026-05-06T11:30:50.114Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266d89010d71a9e121c6'),
+    id: 1,
+    type: 'ax1',
+    waarde: 2,
+    tijd: ISODate('2026-05-06T11:30:53.314Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266d89010d71a9e121c8'),
+    id: 1,
+    type: 'ay1',
+    waarde: 56,
+    tijd: ISODate('2026-05-06T11:30:53.523Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb266d89010d71a9e121ca'),
+    id: 1,
+    type: 'az1',
+    waarde: 0,
+    tijd: ISODate('2026-05-06T11:30:53.716Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267089010d71a9e121cc'),
+    id: 1,
+    type: 'ax1',
+    waarde: 48,
+    tijd: ISODate('2026-05-06T11:30:56.909Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267189010d71a9e121ce'),
+    id: 1,
+    type: 'ay1',
+    waarde: 10,
+    tijd: ISODate('2026-05-06T11:30:57.119Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267189010d71a9e121d0'),
+    id: 1,
+    type: 'az1',
+    waarde: 80,
+    tijd: ISODate('2026-05-06T11:30:57.311Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267489010d71a9e121d2'),
+    id: 1,
+    type: 'ax1',
+    waarde: 7,
+    tijd: ISODate('2026-05-06T11:31:00.510Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267489010d71a9e121d4'),
+    id: 1,
+    type: 'ay1',
+    waarde: 43,
+    tijd: ISODate('2026-05-06T11:31:00.710Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267489010d71a9e121d6'),
+    id: 1,
+    type: 'az1',
+    waarde: 35,
+    tijd: ISODate('2026-05-06T11:31:00.911Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267889010d71a9e121d8'),
+    id: 1,
+    type: 'ax1',
+    waarde: 61,
+    tijd: ISODate('2026-05-06T11:31:04.111Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267889010d71a9e121da'),
+    id: 1,
+    type: 'ay1',
+    waarde: 99,
+    tijd: ISODate('2026-05-06T11:31:04.311Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267889010d71a9e121dc'),
+    id: 1,
+    type: 'az1',
+    waarde: 19,
+    tijd: ISODate('2026-05-06T11:31:04.510Z'),
+    __v: 0
+  },
+  {
+    _id: ObjectId('69fb267b89010d71a9e121de'),
+    id: 1,
+    type: 'ax1',
+    waarde: 31,
+    tijd: ISODate('2026-05-06T11:31:07.713Z'),
+    __v: 0
+  }
+]
+
+
+
+
+
+
+
+[
+  {
+    _id: ObjectId('6a1586a68caa46c1f59df8a3'),
+    cartID: 1,
+    sensoren: [ 1, 2, 3, 4 ]
+  }
+]
+
